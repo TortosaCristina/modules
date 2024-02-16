d00405be814586a24dab27d12b994e576f74884e
TortosaCristina <rtortosacristina@gmail.com>
2024-02-16 15:31:23 +0000

    Tarea 3 - Sin comentarios autogenerados

--- medicos_pacientes/models/diagnostico.py
+++ medicos_pacientes/models/diagnostico.py
@@ -1,20 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-
-# class medicos_pacientes(models.Model):
-#     _name = 'medicos_pacientes.medicos_pacientes'
-#     _description = 'medicos_pacientes.medicos_pacientes'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
 class diagnostico(models.Model):
     _name = 'medicos_pacientes.diagnostico'
     _description = 'medicos_pacientes.diagnostico'
--- medicos_pacientes/models/pacientes.py
+++ medicos_pacientes/models/pacientes.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-
-# class medicos_pacientes(models.Model):
-#     _name = 'medicos_pacientes.medicos_pacientes'
-#     _description = 'medicos_pacientes.medicos_pacientes'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
 class pacientes(models.Model):
     _rec_name = 'nombreCompleto'
     _name = 'medicos_pacientes.pacientes'
