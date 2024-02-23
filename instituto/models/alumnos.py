395fa8342fe17321b0a6b8580fdd72299cca933e
TortosaCristina <rtortosacristina@gmail.com>
2024-02-23 15:18:53 +0000

    Tarea 4 - Campos related añadidos

--- instituto/models/alumnos.py
+++ instituto/models/alumnos.py
@@ -10,3 +10,13 @@ class alumnos(models.Model):
     apellidos = fields.Text()
     dni = fields.Text()
     modulos = fields.Many2many(comodel_name='instituto.modulos')
+
+    primerProfesor = fields.Many2one(comodel_name='instituto.profesores',
+                                     related='modulos.profesor',
+                                     string='Primer Profesor',
+                                     readonly=True)
+    
+    listaModulos = fields.One2many('instituto.modulos',
+                                   related='primerProfesor.modulos',
+                                   string='Modulos',
+                                   readonly=True)
